e0356585ec5b06a88cbfc6ebffe1daa81dd17f93
Ahmad Amireh <ahmad@amireh.net>
2017-08-30 10:37:51 +0300

    use megadoc v6 for docs

---
+++ docs/megadoc-assets/megadoc-theme-minimalist.js
@@ -0,0 +1,1 @@
+exports["megadoc-theme-minimalist"]=function(e){function t(o){if(r[o])return r[o].exports;var n=r[o]={exports:{},id:o,loaded:!1};return e[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var r={};return t.m=e,t.c=r,t.p="",t(0)}([function(e,t,r){e.exports=r(1)},function(e,t){"use strict";e.exports={type:"theme",name:"megadoc-theme-minimalist"}}]);
